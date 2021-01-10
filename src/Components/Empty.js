05167f531b4c8adef2f6557c3da4cc3ea52995ab
umer2001 <umer2001.uf@gmail.com>
2021-01-10 23:22:15 +0500

    add: empty screen

---
+++ src/Components/Empty.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Grid from "@material-ui/core/Grid";
+import empty from "../Images/empty.svg";
+
+export const Empty = () => {
+  return (
+    <Grid container className="container">
+      <Grid item className="grid-item">
+        <img src={empty} className="empty-img" />
+        <h2>You're all done for today!</h2>
+        <h2>#TodoitZero</h2>
+      </Grid>
+    </Grid>
+  );
+};
+
+export default Empty;
