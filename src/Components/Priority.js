1a87631b3e3e540a5998004ec00644163bfd41d9
umer2001 <umer2001.uf@gmail.com>
2021-01-11 20:43:01 +0500

    fix: linting errors

--- src/Components/Priority.js
+++ src/Components/Priority.js
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import { makeStyles } from "@material-ui/core/styles";
 import Popover from "@material-ui/core/Popover";
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
