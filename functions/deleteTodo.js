aceece309829a0cbdce8658f0ec667e9b08d81ef
umer2001 <umer2001.uf@gmail.com>
2021-02-03 00:31:53 +0500

    add: deletes all sub todos when parent id deleted

--- functions/deleteTodo.js
+++ functions/deleteTodo.js
@@ -2,20 +2,32 @@ const { DELETE_TODO, GET_TODO_BY_ID } = require("./utils/todoQuries");
 const sendQuery = require("./utils/sendQuries");
 const formattedResponse = require("./utils/formattedResponse");
 
-exports.handler = async (event) => {
-  if (event.httpMethod !== "DELETE") {
-    return formattedResponse(405, { err: "Method not supported" });
-  }
-
-  const { id } = JSON.parse(event.body);
+async function recursiveDelete(id) {
+  // find all sub toods for the (parent) id
   const variables = { id };
-  try {
-    const { findTodoByID: Todo } = await sendQuery(GET_TODO_BY_ID, variables);
-    Todo.subtodos.forEach((subTodo) => {
-      sendQuery(DELETE_TODO, { id: subTodo._id });
+  const { findTodoByID: Todo } = await sendQuery(GET_TODO_BY_ID, variables);
+  // save sub todos of (parent) id
+  console.log(Todo.subtodos);
+  // delete todo with (parent) id
+  const { deleteTodo: deletedTodo } = await sendQuery(DELETE_TODO, variables);
+  // now check if saved sub todos are more than 0
+  if (Todo.subtodos.length > 0) {
+    // call the same finction with each saved sub todo id
+    Todo.subtodos.forEach(async (subTodo) => {
+      await recursiveDelete(subTodo._id);
     });
-    const { deleteTodo: deletedTodo } = await sendQuery(DELETE_TODO, variables);
-    return formattedResponse(200, deletedTodo);
+  } else {
+    // there is no sub todo under this id
+    return;
+  }
+}
+
+exports.handler = async (event, context, callback) => {
+  try {
+    const { id } = JSON.parse(event.body);
+    console.log("got : ", id);
+    await recursiveDelete(id);
+    return formattedResponse(200, { body: "deleting..." });
   } catch (err) {
     console.error(err);
     return formattedResponse(500, { err: "Something went wrong" });
