58b85bb1c966f83ffe550da0792355d4fbef3035
umer2001 <umer2001.uf@gmail.com>
2021-03-28 19:13:09 +0500

    add: settings page

--- src/Setting.js
+++ src/Setting.js
@@ -1,9 +1,22 @@
 import React from "react";
-import { List, ListItem, ListItemIcon, ListItemText } from "@material-ui/core";
+import {
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+  makeStyles,
+} from "@material-ui/core";
 import PaletteOutlinedIcon from "@material-ui/icons/PaletteOutlined";
 import { Link } from "react-router-dom";
 
+const useStyles = makeStyles((theme) => ({
+  listItemText: {
+    color: theme.palette.text.primary,
+  },
+}));
+
 const Setting = () => {
+  const classes = useStyles();
   return (
     <List>
       <Link to="/theme" className="link">
@@ -11,7 +24,7 @@ const Setting = () => {
           <ListItemIcon>
             <PaletteOutlinedIcon />
           </ListItemIcon>
-          <ListItemText primary="Theme" />
+          <ListItemText primary="Theme" className={classes.listItemText} />
         </ListItem>
       </Link>
     </List>
